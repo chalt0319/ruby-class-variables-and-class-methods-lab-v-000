b245df504760c564d1aa14da6f599104b6bb659a
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-22 18:53:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,13 +28,14 @@ class Song
   def self.genre_count 
     hash = {}
     @@genres.each do |genre|
-      binding.pry 
+      
       if hash.keys.include?(genre)
         hash[genre] += 1
       else 
         hash[genre] = 1 
       end 
     end 
+    binding.pry 
     hash 
   end 
 end 
