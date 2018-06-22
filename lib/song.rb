1895036b9f2f4b387be572dce98227112e23a1aa
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-22 17:38:36 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,13 @@
 class Song 
   attr_accessor :name, :artist, :genre 
   @@count = 0 
+  @@artists = []
   def initialize(name, artist, genre)
     @name = name 
     @artist = artist
     @genre = genre
     @@count += 1 
+    @@artists << @artist 
   end 
   
   def count
